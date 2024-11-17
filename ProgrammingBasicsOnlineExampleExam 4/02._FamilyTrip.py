2d07ef25493bf44ccd76acdb12f00b7849e29efa
mihailcenov <mihailceov@gmail.com>
2024-11-17 16:17:11 +0200

    Resolved error in comparison of family_budget with total_money.

--- ProgrammingBasicsOnlineExampleExam 4/02._FamilyTrip.py
+++ ProgrammingBasicsOnlineExampleExam 4/02._FamilyTrip.py
@@ -4,14 +4,14 @@ price_per_night = float(input())
 percent_extra_costs = int(input())
 
 if number_nights > 7:
-    price_per_night -= (price_per_night * 5) / 100
+    price_per_night -= (price_per_night * 5.0) / 100.0
 
 
 price_extra_costs = (family_budget * percent_extra_costs) / 100
 total_money = (price_per_night * number_nights) + price_extra_costs
 # print(total_money)
 
-if family_budget > total_money:
+if family_budget >= total_money:
     money_left = family_budget - total_money
     # print(money_left)
     print(f"Ivanovi will be left with {money_left:.2f} leva after vacation.")
